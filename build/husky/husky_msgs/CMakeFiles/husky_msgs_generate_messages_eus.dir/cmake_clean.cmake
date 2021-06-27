84c7dea05af0775406590c1e5afe4873e69604ad
Vivek Agrawal <vivekagr@iitk.ac.in>
2021-06-27 16:44:23 +0530

    Initial Commit

---
+++ build/husky/husky_msgs/CMakeFiles/husky_msgs_generate_messages_eus.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/vivek/husky/devel/share/roseus/ros/husky_msgs/manifest.l"
+  "/home/vivek/husky/devel/share/roseus/ros/husky_msgs/msg/HuskyStatus.l"
+  "CMakeFiles/husky_msgs_generate_messages_eus"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/husky_msgs_generate_messages_eus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
